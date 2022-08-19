6fe7a8122c1f013a4b8f92fec53ddb2af3fee0c8
ShyamGurunath <shyamgurunath474@gmail.com>
2022-08-19 22:22:37 +0530

    Added Button Component

--- shineloggerui/pages/index.tsx
+++ shineloggerui/pages/index.tsx
@@ -1,13 +1,35 @@
 import type { NextPage } from 'next'
+import { useEffect,useState,useCallback,useContext,useMemo } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0)
+
+    const increment = useCallback(() => {
+        setCount(prevState => {
+            return prevState + 1
+        })
+    }
+    ,[count])
+
+    const decrement = useCallback(() => {
+        if(count > 0){
+            setCount(prevState => {
+                return prevState - 1
+            })
+        }
+    }   ,[count])
+
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+  <>
+    <div className={`flex justify-center`}>
+      <h2 className={`text-7xl`}>{count}</h2>
+        <button onClick={increment} className={`text-5xl text-blue-50 rounded pl-5 pr-5 pt-1 pb-1 m-2 border-amber-900 border-2 bg-green-300`}>+</button>
+        <button onClick={decrement} className={`text-5xl text-blue-50 rounded pl-5 pr-5 pt-1 pb-1 m-2 border-red-600 border-2 bg-red-400`}>-</button>
+    </div>
+  </>
   )
 }
 
